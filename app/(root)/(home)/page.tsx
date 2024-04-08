9f70dc45089a9e04627b2417b0cef4969df20f0d
Joseph23-11 <joseph.simarmata2311@mail.ugm.ac.id>
2024-04-08 22:03:54 +0700

    add homepage and HomePageFilters

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,24 @@
+import HomeFilters from "@/components/home/HomeFilters";
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            Ask a Question
+          </Button>
+        </Link>
+      </div>
+
+      <HomeFilters />
+    </>
+  );
+};
+
+export default Home;
